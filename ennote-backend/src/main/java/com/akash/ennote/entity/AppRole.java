ea0964d2925f9096341b7b20e9959b7fabe10fa6
Aakash <iakashman8653@gmail.com>
2024-08-18 23:14:54 +0530

    Done User Management And Tested

---
+++ ennote-backend/src/main/java/com/akash/ennote/entity/AppRole.java
@@ -0,0 +1,6 @@
+package com.akash.ennote.entity;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ ennote-backend/src/main/java/com/akash/ennote/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.akash.ennote.repository;
+
+import com.akash.ennote.entity.AppRole;
+import com.akash.ennote.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+   Optional<Role> findByRoleName(AppRole appRole);
+}
